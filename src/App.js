9ba9b2fd8b3c738185fb7896ddc2720342dd4e6f
techieinovators <techie.inovators@gmail.com>
2020-03-01 12:06:46 +0000

    Make change to the repo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <h2>Start writing code!</h2>
+      <h2>Start writing code in this component!</h2>
     </div>
   );
 }
